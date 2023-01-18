9a3714a570ea14fcbc6d742e562b203a34a19419
Nicolás D'Agostino <nicolasdagostino@gmail.com>
2023-01-18 16:02:16 +0100

    Delete src directory

--- src/app/components/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'app-header',
-  //template: `<h2>Cabecera de la aplicacion <h2>`
-  templateUrl: './header.component.html'
-})
-
-export class HeaderComponent{}
